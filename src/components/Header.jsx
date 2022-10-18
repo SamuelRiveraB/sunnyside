e07f540ec39226e5a49ab1fccf2992b98be9abbd
Samuel Rivera <114023422+SamuelRiveraB@users.noreply.github.com>
2022-10-18 14:49:01 -0500

    Add initial files

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+import nav from "../images/icon-hamburger.svg";
+
+function Header() {
+    function navBar() {
+        const navImg = document.getElementsByClassName("nav-btn")[0];
+        const right = document.getElementsByClassName("right-header")[0];
+        if (right.style.visibility === "hidden") {
+            right.style.visibility = "visible";
+        } else {
+            right.style.visibility = "hidden";
+            navImg.style.visibility = "visible";
+        }
+    }
+
+    return (
+        <>
+            <nav className="header">
+                <div className="left-header">
+                    <h1 className="logo">sunnyside</h1>
+                </div>
+                <div className="right-header">
+                    <div className="triangle"></div>
+                    <img src={nav} className="nav-btn" alt="nav" onClick={navBar}/>
+                    <ul className="header-menus">
+                        <li><a href="/" className="menu">About</a></li>
+                        <li><a href="/" className="menu">Services</a></li>
+                        <li><a href="/" className="menu">Projects</a></li>
+                        <li><button>Contact</button></li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+
+    return (
+        <div>
+            <div className="container">
+                <div className="left-side">
+                    <p className="pre-space">SO, YOU WANT TO TRAVEL TO</p>
+                    <h1 className="space">SPACE</h1>
+                    <p className="post-space">Let’s face it; if you want to go to space, you might as well genuinely go to 
+                        outer space and not hover kind of on the edge of it. Well sit back, and relax 
+                        because we’ll give you a truly out of this world experience!
+                    </p>
+                </div>
+                <div className="right-side">
+                    <div className="circle">
+                        <h2 className="explore">EXPLORE</h2>
+                    </div>
+                </div>
+                We are creatives
+
+  Transform your brand
+
+  We are a full-service creative agency specializing in helping brands grow fast. 
+  Engage your clients through compelling visuals that do most of the marketing for you.
+
+  Learn more
+
+  Stand out to the right audience
+
+  Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. 
+
+  Learn more
+
+  Graphic design
+  Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.
+
+  Photography
+  Increase your credibility by getting the most stunning, high-quality photos that improve your business image.
+
+  Client testimonials
+
+  We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.
+  Emily R.
+  Marketing Director
+
+  Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.
+  Thomas S.
+  Chief Operating Officer
+
+  Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!
+  Jennie F.
+  Business Owner
+
+  About
+  Services
+  Projects
+  Contact
+            </div>
+        </div>
+    );
+}
+
+export default Home;
